488212aae69bb6d1c027460b8f7d479e4bd310a0
Jiongan Mu <max1078735979@gmail.com>
2021-05-26 11:23:43 -0400

    ContactForm.js completed

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,7 +9,24 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+  const handleNameChange = e => {
+    setName(e.target.value);
+  }
+
+  const handlePhoneChange = e => {
+    setPhone(e.target.value);
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" value={name} placeholder="Enter your name" onChange={handleNameChange}/>
+      <input type="tel" name="phone" value={phone} placeholder="Enter your phone number" pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" onChange={handlePhoneChange}/>
+      <input type="email" name="email" value={email} placeholder="Enter your email" onChange={handleEmailChange}/>
+      <input type="submit" value='submit'/>
+    </form>
   );
 };
